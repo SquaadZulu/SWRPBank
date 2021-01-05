fab27aff67190a49298a23d3cc9cd34da3c6ceca
SquaadZulu <dwerp5956@gmail.com>
2021-01-05 17:40:58 -0500

    Update Pcalc.php

--- debug/Pcalc.php
+++ debug/Pcalc.php
@@ -7,14 +7,14 @@
 <div>
 <h1>Persistent Calculator</h1>
 </div>
-    <form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         <input name="v1"/>
         <select name="action"/>
             <option value="add">Deposit</option>
             <option value="subtract">Withdraw</option>
         <input name="v2"/>
 
-        <input type="submit" name="Submit"/>
+        <input type="submit" name="Submit" value="submit"/>
     </form>
 </body>
 </html>
