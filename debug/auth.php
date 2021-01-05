f904e1ae03e7bdebe967e03b0e1f2be63bf182ad
SquaadZulu <dwerp5956@gmail.com>
2021-01-04 23:16:52 -0500

    Update auth.php

--- debug/auth.php
+++ debug/auth.php
@@ -9,5 +9,17 @@
 </div>
 <input type="text" id="PC" name="passcode">
 <input type="submit"  value="Auth">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<?php
+// define variables and set to empty values
+$passcode = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $passcode = test_input($_POST["passcode"]);
+}
+if ($passcode = "88224646") {
+    echo yeet;
+}
+?>
 </body>
 </html>
