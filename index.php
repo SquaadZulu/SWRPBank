a9b5f55e8d66042b88ec95bc65dbf3092591d934
SquaadZulu <dwerp5956@gmail.com>
2021-01-04 22:04:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <title>Project GreenMat[DEV]</title>
 <body>
 <div>
-<h1> Backend Test 1: PersistentCalc </h1> <href = "/debug/auth.php">
+<h1> Backend Test 1: PersistentCalc </h1> 
 </div>
-
+<link rel="help" href="/debug/auth.php"
 </body>
 </html>
